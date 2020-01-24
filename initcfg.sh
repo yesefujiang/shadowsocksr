ae7e0fd1bf73a1e64f00399d58333b1e3ea1ced4
root <he123789456@gmail.com>
2020-01-25 01:38:30 +0800

    init

---
+++ initcfg.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+chmod +x *.sh
+wget https://github.com/nakagami/CyMySQL/archive/REL_0_9_4.tar.gz
+tar -zxvf REL_0_9_4.tar.gz >>/dev/zero
+mv CyMySQL-REL_0_9_4 CyMySQL
+rm -rf cymysql
+mkdir cymysql
+mv CyMySQL/cymysql ./
+rm -rf CyMySQL
+rm -rf REL_0_9_4.tar.gz
+chmod +x shadowsocks/*.sh
+cp -n apiconfig.py userapiconfig.py
+cp -n config.json user-config.json
+cp -n mysql.json usermysql.json
+
